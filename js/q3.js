970b4719364cc2426c21d0a1f6c652154f8d6c03
Omar <omx302@gmail.com>
2021-04-17 23:51:22 +0300

    q3 done

--- js/q3.js
+++ js/q3.js
@@ -2,12 +2,12 @@ for (var i = 0; i <20; i++) {
     if(i>=0)
     {
         console.log(i+" is Even")
-        document.write("<h1>"+num1+" is Even</h1>");
+        document.write("<h1>"+i+" is Even</h1>");
     }
     else
     {
         console.log(i+" is Odd")
-        document.write("<h1>"+num1+" is Odd</h1>");
+        document.write("<h1>"+i+" is Odd</h1>");
     }
     
 }
