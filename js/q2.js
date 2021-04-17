2e5f4d5bca84a245bf241a77d31c519f6a0297a9
Omar <omx302@gmail.com>
2021-04-17 23:31:48 +0300

    q2 done

--- js/q2.js
+++ js/q2.js
@@ -4,17 +4,19 @@ num1= prompt("Enter First Number:");
 num2= prompt("Enter Second Number:");
 num3= prompt("Enter Third Number:");
 
-if(num1>num2 && num1>num3)
+
+
+if(num1>=num2 && num1>=num3)
 {
     console.log("Max number is "+num1)
     document.write("<h1>Max number is "+num1+"</h1>");
 }
-else if(num2>num1 && num2>num3)
+else if(num2>=num1 && num2>=num3)
 {
     console.log("Max number is "+num2)
     document.write("<h1>Max number is "+num2+"</h1>");
 }
-else if(num3>num1 && num3>num2)
+else if(num3>=num1 && num3>=num2)
 {
     console.log("Max number is "+num2)
     document.write("<h1>Max number is "+num3+"</h1>");
