9da9df1ab4f9c36f778cbd2f518719d46be10c5a
Omar <omx302@gmail.com>
2021-04-18 00:06:22 +0300

    q1 update

--- js/q1.js
+++ js/q1.js
@@ -1,7 +1,11 @@
 var num=prompt("Enter any Number :");
 if(num>=0){
     alert("This Number is +");
+    console.log("This Number is +")
+    document.write("<h1>This Number is + </h1>");
 }
 else{
     alert("This Number is -");
+    console.log("This Number is -")
+    document.write("<h1>This Number is - </h1>");
 }
