5b4713a690bdeb3119f2ef9b26992bf063fa4a8e
Omar Alazaizeh <omx302@gmail.com>
2021-05-12 02:13:49 +0300

    final

--- pages/201/week2/js/app.js
+++ pages/201/week2/js/app.js
@@ -13,28 +13,29 @@ buttonOne.onclick = function () {
   buttonOne.style.background = "#c99";
   buttonTwo.style.background = "#b9bbbd";
   buttonThree.style.background = "#b9bbbd";
-  img1.style.backgroundImage = 'url("imgs/gallery-item-01.jpg")';
-  img2.style.backgroundImage = 'url("imgs/gallery-item-02.jpg")';
-  img3.style.backgroundImage = 'url("imgs/gallery-item-03.jpg")';
-  img4.style.backgroundImage = 'url("imgs/gallery-item-04.jpg")';
+
+  img1.src = "imgs/gallery-item-01.jpg";
+  img2.src = "imgs/gallery-item-02.jpg";
+  img3.src = "imgs/gallery-item-03.jpg";
+  img4.src = "imgs/gallery-item-04.jpg";
 };
 buttonTwo.onclick = function () {
   buttonOne.style.background = "#b9bbbd";
   buttonTwo.style.background = "#c99";
   buttonThree.style.background = "#b9bbbd";
 
-  img1.style.backgroundImage = 'url("imgs/gallery-item-03.jpg")';
-  img2.style.backgroundImage = 'url("imgs/gallery-item-04.jpg")';
-  img3.style.backgroundImage = 'url("imgs/gallery-item-05.jpg")';
-  img4.style.backgroundImage = 'url("imgs/gallery-item-06.jpg")';
+  img1.src = "imgs/gallery-item-03.jpg";
+  img2.src = "imgs/gallery-item-04.jpg";
+  img3.src = "imgs/gallery-item-05.jpg";
+  img4.src = "imgs/gallery-item-06.jpg";
 };
 buttonThree.onclick = function () {
   buttonOne.style.background = "#b9bbbd";
   buttonTwo.style.background = "#b9bbbd";
   buttonThree.style.background = "#c99";
 
-  img1.style.backgroundImage = 'url("imgs/gallery-item-05.jpg")';
-  img2.style.backgroundImage = 'url("imgs/gallery-item-06.jpg")';
-  img3.style.backgroundImage = 'url("imgs/gallery-item-07.jpg")';
-  img4.style.backgroundImage = 'url("imgs/gallery-item-08.jpg")';
+  img1.src = "imgs/gallery-item-05.jpg";
+  img2.src = "imgs/gallery-item-06.jpg";
+  img3.src = "imgs/gallery-item-07.jpg";
+  img4.src = "imgs/gallery-item-08.jpg";
 };
